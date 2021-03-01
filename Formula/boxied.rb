2f0fcea7c150d5c4977d4944db77eb00a307b144
Mark Wardle <mwardle@robinsage.com>
2021-03-01 14:42:32 -0700

    Bump version

--- Formula/boxied.rb
+++ Formula/boxied.rb
@@ -1,9 +1,9 @@
 class Boxied < Formula
-  version "1.0.5"
+  version "1.0.6"
   sha256 "c1bf6ab9717e0d1490eaaeb2c63215b319b71c880bce3b48f9b37febb5553151"
   desc "Local print server for boxie.io"
   homepage "https://boxie.io"
-  url "https://boxie-dev.s3.amazonaws.com/software/boxied/versions/boxied-macos-x64-v1-0-5.tar.gz"
+  url "https://boxie-dev.s3.amazonaws.com/software/boxied/versions/boxied-macos-x64-v1-0-6.tar.gz"
   license "Proprietary"
 
   def install
