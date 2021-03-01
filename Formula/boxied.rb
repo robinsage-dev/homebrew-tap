dbfbe8dfdbfeaa8546b059c44ca258abc89a2a6c
Mark Wardle <mwardle@robinsage.com>
2021-03-01 14:46:45 -0700

    Update sha256

--- Formula/boxied.rb
+++ Formula/boxied.rb
@@ -1,6 +1,6 @@
 class Boxied < Formula
   version "1.0.6"
-  sha256 "c1bf6ab9717e0d1490eaaeb2c63215b319b71c880bce3b48f9b37febb5553151"
+  sha256 "8c9deb5b8122f491ad23a88a380c1f8dfa1d99bb387adc4e77d44527664eabf7"
   desc "Local print server for boxie.io"
   homepage "https://boxie.io"
   url "https://boxie-dev.s3.amazonaws.com/software/boxied/versions/boxied-macos-x64-v1-0-6.tar.gz"
